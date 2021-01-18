eaa59a2137bbcc766c19c19c4231b42b4ee9e52f
bartoszgurgul <bartoszgurgul@gmail.com>
2021-01-18 14:26:46 +0100

    Skonfigurowanie polaczenia w takie sposób aby - pozbyc sie pliku persistence na dobre  zamiast tego mamy wszedlkie konfiguracje poukrywane w JpaConfig i postaci  Druga sprawa zmiana wczesniejszej klasy na LocalContainerEntityManagerFacyoryBean która wymaga JpaVendorAdaptore  w postaci hibernate

--- src/main/java/pl/javastart/config/JpaConfig.java
+++ src/main/java/pl/javastart/config/JpaConfig.java
@@ -2,14 +2,40 @@ package pl.javastart.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class JpaConfig {
     @Bean
-    public LocalEntityManagerFactoryBean createEMF() {
-        LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
-        emf.setPersistenceUnitName("spring-jpa-pu");
-        return emf;
+    public LocalContainerEntityManagerFactoryBean createEMF(JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/library?useSSL=false&serverTimezone=UTC");
+        properties.put("javax.persistence.jdbc.user","root");
+        properties.put("javax.persistence.jdbc.password","root");
+        properties.put("javax.persistence.jdbc.driver","com.mysql.jdbc.Driver");
+        properties.put("javax.persistence.schema-generation.database.action","drop-and-create");
+
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("spring-jpa-pu");
+        localContainerEntityManagerFactoryBean.setJpaPropertyMap(properties);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("pl.javastart.model");
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaVendorAdapter createVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter =
+                new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        hibernateJpaVendorAdapter.setShowSql(true);
+        return hibernateJpaVendorAdapter;
     }
 }
